57b56ed49e612bb253c728a0c7666ca6eded5f19
dimka11 <dimka11lp@gmail.com>
2017-11-24 23:16:33 +0600

    Refactor GeneralMatrix

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,41 +1,53 @@
 #pragma warning( disable : 4290 )  
 #include "Matrix.h"
 #include "OneDArray.h"
+using namespace std;
 
-// Структура указателей на функции и тело матрицы
+// pointers:
 struct Matrix{
 	void* body;
-	p_Of_get_ij _get_ij; // Указатели
+	p_Of_get_ij _get_ij;
 	p_Of_set_ij _set_ij;
 	p_Of_delete _delete;
 	p_Of_size _size;
 };
 
-Matrix* createMatrix(void *body, p_Of_get_ij _get_ij, p_Of_set_ij _set_ij, p_Of_delete _delete, p_Of_size _size)
+Matrix* CreateGeneralOneDMatrix(size_t s)
 {
-	Matrix* res = new Matrix; // выделяем память под матрицу и функции
-	res->body = body;
+	Matrix  *GeneralMatrix = new Matrix();
+	GeneralMatrix->body = CreateOneDMatrix(s);
+	GeneralMatrix->_get_ij = get_ijOneDMatrix;
+	GeneralMatrix->_set_ij = set_ijOneDMatrix;
+	GeneralMatrix->_delete = deleteOneDMatrix;
+	GeneralMatrix->_size = sizeOneDMatrix;
 
-	res->_get_ij = _get_ij;
-	res->_set_ij = _set_ij;
-	res->_delete = _delete;
-	res->_size = _size;
+	return GeneralMatrix;
+}
 
-	return res; 
-}
-double get_ij(Matrix& m, size_t i, size_t j)
+Matrix *CreateGeneralTwoDMatrix(size_t s)
 {
-	return m._get_ij(m.body, i, j);
+	Matrix *GeneralMatrix = new Matrix();
+	GeneralMatrix->body = CreateGeneralTwoDMatrix(s);
+	GeneralMatrix->_get_ij = get_ijTwoDMatrix;
+	GeneralMatrix->_set_ij = set_ijOneDMatrix;
+	GeneralMatrix->_delete = deleteOneDMatrix;
+	GeneralMatrix->_size = sizeTwoDMatrix;
+	return GeneralMatrix;
 }
-void  set_ij(Matrix& m, size_t i, size_t j, double val)
+string get_ij(Matrix &M, size_t i, size_t j)
 {
-	m._set_ij(m.body, i, j, val);
+	return M._get_ij(M.body, i, j);
 }
-void deleteMatrix(Matrix& m)
+void  set_ij(Matrix &M, size_t i, size_t j, std::string value)
 {
-	m._delete(m.body);
+	M._set_ij(M.body, i, j, value);
 }
-size_t size(Matrix& m)
+void deleteMatrix(Matrix *M)
 {
-	return m._size(m.body);
+	return M->_delete(M->body);
+	//return M._delete(M.body);
+}
+size_t size(Matrix const &M)
+{
+	return M._size(M.body);
 }
--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -2,6 +2,8 @@
 #define _MATRIX_H_
 #include "OneDArray.h"
 #include "TwoDArray.h"
+#include <stdexcept>
+
 
 struct Matrix;
 
@@ -16,7 +18,7 @@ Matrix *CreateGeneralOneDMatrix(size_t s);
 Matrix *CreateGeneralTwoDMatrix(size_t s);
 //Matrix* (void *body, p_Of_get_ij _get_ij, p_Of_set_ij _set_ij, p_Of_delete _delete, p_Of_size _size); // create new matrix
 std::string get_ij(Matrix const &M, size_t i, size_t j); // getter
-void set_ij(Matrix& M, size_t i, size_t j, std::string value); // setter
+void set_ij(Matrix &M, size_t i, size_t j, std::string value); // setter
 void deleteMatrix(Matrix *M) throw(); // delete
 size_t size(Matrix const &M) throw(); // size of matrix
 
