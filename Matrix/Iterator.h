17d627b327067541f909df63147d7d9caa21cd61
dimka11 <dimka11lp@gmail.com>
2017-11-25 01:22:43 +0600

    01:22 commit

--- Matrix/Iterator.h
+++ Matrix/Iterator.h
@@ -1,4 +1,18 @@
-#pragma once
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+#include "Matrix.h"
+
+struct It;
+It *begin(Matrix *Matrix);
+bool next(It &i);
+size_t current(It &i);
+
+
+
+#endif
+
+/*
 struct It;
 void begin(It &it);
 void next(It &it);
@@ -6,3 +20,5 @@ int current(It &it);
 // int current_index(&It it);
 int setto(It &it);
 void deleteIterator(It &it);
+
+*/
