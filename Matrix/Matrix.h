85fb1791da5ea4ca155d51f6c86f9b22cd36924f
dimka11 <dimka11lp@gmail.com>
2017-11-24 22:46:53 +0600

    Refactor header of General Matrix implementation

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -2,22 +2,23 @@
 #define _MATRIX_H_
 #include "OneDArray.h"
 #include "TwoDArray.h"
+
 struct Matrix;
+
 // pointers to functions:
 
 typedef std::string(*p_Of_get_ij)(void*, size_t, size_t)throw(std::invalid_argument);; // getter
 typedef void(*p_Of_set_ij)(void*, size_t, size_t, std::string)throw(std::invalid_argument);; // setter
 typedef void(*p_Of_delete)(void*) throw(); // delete
-typedef size_t(*p_Of_size)(void*) throw(); // size of matrix
+typedef size_t(*p_Of_size)(void*);// size of matrix
 
+Matrix *CreateGeneralOneDMatrix(size_t s);
+Matrix *CreateGeneralTwoDMatrix(size_t s);
+//Matrix* (void *body, p_Of_get_ij _get_ij, p_Of_set_ij _set_ij, p_Of_delete _delete, p_Of_size _size); // create new matrix
+std::string get_ij(Matrix const &M, size_t i, size_t j); // getter
+void set_ij(Matrix& M, size_t i, size_t j, std::string value); // setter
+void deleteMatrix(Matrix *M) throw(); // delete
+size_t size(Matrix const &M) throw(); // size of matrix
 
-Matrix* createMatrix(void *body, p_Of_get_ij _get_ij, p_Of_set_ij _set_ij, p_Of_delete _delete, p_Of_size _size); // Создание новой матрицы
-double get_ij(Matrix& m, size_t i, size_t j); // getter
-void  set_ij(Matrix& m, size_t i, size_t j, double val); // setter
-void deleteMatrix(Matrix& m) throw(); // Удаление матрицы
-size_t size(Matrix& m) throw(); // Размер матрицы
-
-bool CheckThatIndexIsInRange(Matrix &m, size_t j); 
 
 #endif
-/////s
